7f9dc26d9d47c7743d34a6e4336ace13b382678f
Dark Arts and Sciences <darkartsandsciences@gmail.com>
2016-04-16 14:16:05 -0700

    Gradient background, others shapeshift to match when looked at.

--- ld35.js
+++ ld35.js
@@ -12,6 +12,7 @@ window.onload = function() {
 	var playerLook = 0;
 	
 	var titleHeight = 65;
+	var bgImageData;
 	
 	var playerSize = 16;
 	var viewDistance = 4;
@@ -23,10 +24,12 @@ window.onload = function() {
  
     function init() {
 		spawn();
+		
         canvas.addEventListener("mousemove", onMouseMove);
         canvas.addEventListener("mousedown", onMouseDown);
         canvas.addEventListener("mouseup", onMouseUp);
         canvas.addEventListener("mouseout", onMouseOut);
+		
         main(0);
     }
  
@@ -54,34 +57,69 @@ window.onload = function() {
     }
 	
 	function spawn() {
-		for (var i=0; i<11; i++) {
-			others[i] = {
-				x:  Math.random() * canvas.width, 
-				y: (Math.random() * canvas.height - titleHeight) + titleHeight
+		// background pattern
+		bgImageData = context.createImageData(canvas.width, canvas.height - titleHeight);
+		for (var iy = 0; iy < canvas.height - titleHeight; iy++) {
+			for (var ix = 0; ix < canvas.width; ix++) {
+				var i = (ix + iy * bgImageData.width) * 4;
+				bgImageData.data[i+0] = (ix - iy) % 256;  // r
+				bgImageData.data[i+1] = (ix + iy) % 256;  // g
+				bgImageData.data[i+2] = 255;  // b
+				bgImageData.data[i+3] = 255;  // a
 			}
+		}
+		context.putImageData(bgImageData, 0, titleHeight);
+		
+		// others
+		var size = 8;
+		for (var id=0; id < 23; id++) {
+			others[id] = { 
+				x: Math.floor( Math.random() *  (canvas.width  - size              ) + size                ),
+				y: Math.floor( Math.random() * ((canvas.height - size - titleHeight) + size + titleHeight) ),
+				size: size, 
+				imageData: null,
+				minMoveDelay:  1,  // frames
+				maxMoveDelay: 10,
+				nextMove: 0
+			};
+			move(others[id]);
+			shapeshift(others[id]);
 		}
 	}
  
     function render() {
-        drawFrame();
-		drawOthers();
-		drawPlayer();
-    }
-	
-	function drawOthers() {
-		others.forEach(function(other) {
+        // background
+		context.putImageData(bgImageData, 0, titleHeight);
+ 
+		// title
+        context.fillStyle = "#333333";
+        context.fillRect(0, 0, canvas.width, titleHeight);
+ 
+        context.fillStyle = "white";
+        context.font = "24px Verdana";
+        context.fillText("LD35 ShapeShift", 10, 30);
+ 
+        context.font = "12px Verdana";
+        context.fillText("Fps: " + fps, 13, 50);
+        context.fillText("Mouse: " + mousePos.x + ", " + mousePos.y, 103, 50);
+		
+		// others
+		others.forEach(function (other) {
 			if (isInView(other.x, other.y)) {
-				drawOther("green", 2, other.x, other.y);
+				shapeshift(other);
+				drawOther(other, "green");
 			} else {
-				other.x += Math.round(Math.random() * 4) - 2;
-				other.y += Math.round(Math.random() * 4) - 2;
-				drawOther("red", 2, other.x, other.y);
+				move(other);
+				drawOther(other, "red");
 			}
 		});
-	}
-	
+		
+		// player
+		drawPlayer();
+    }
+		
 	function isInView(x, y) {
-		var viewDir = Math.PI*3/2 - getDir(playerX, playerY, x, y + titleHeight);
+		var viewDir = Math.PI*3/2 - angleBetween(playerX, playerY, x, y);
 		
 		http://stackoverflow.com/questions/1878907/the-smallest-difference-between-2-angles
 		var diff = playerLook - viewDir;
@@ -90,9 +128,9 @@ window.onload = function() {
 		return (Math.abs(diffAngle) < playerFOV/2);
 	}
 	
-	function drawOther(color, size, x, y) {
-		context.fillStyle = color;
-		context.fillRect(x, y + titleHeight, size, size);
+	function drawOther(other, color) {
+		var s2 = Math.floor(other.size / 2);
+		context.putImageData(other.imageData, other.x - s2, other.y - s2);
 	}
 	
 	function drawPlayer() {
@@ -129,43 +167,40 @@ window.onload = function() {
 		context.fillRect(mousePos.x, mousePos.y, 2, 2);
 	}
 	
-    function drawFrame() {
-        context.fillStyle = "#dddddd";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        context.fillStyle = "#eeeeee";
-        context.fillRect(1, 1, canvas.width-2, canvas.height-2);
- 
-        context.fillStyle = "#333333";
-        context.fillRect(0, 0, canvas.width, titleHeight);
- 
-        context.fillStyle = "white";
-        context.font = "24px Verdana";
-        context.fillText("LD35 ShapeShift", 10, 30);
- 
-        context.font = "12px Verdana";
-        context.fillText("Fps: " + fps, 13, 50);
-        context.fillText("Mouse: " + mousePos.x + ", " + mousePos.y, 103, 50);
-    }
- 
+
     function onMouseMove(e) {
-        mousePos = getMousePos(canvas, e);
-		playerLook = Math.PI*3/2 - getDir(playerX, playerY, mousePos.x, mousePos.y);
+		var rect = canvas.getBoundingClientRect();
+        mousePos.x = Math.round((e.clientX - rect.left)/(rect.right - rect.left)*canvas.width);
+		mousePos.y = Math.round((e.clientY - rect.top)/(rect.bottom - rect.top)*canvas.height);
+		playerLook = Math.PI*3/2 - angleBetween(playerX, playerY, mousePos.x, mousePos.y);
 	}
     function onMouseDown(e) {}
     function onMouseUp(e) {}
     function onMouseOut(e) {}
- 
-    function getMousePos(canvas, e) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-            x: Math.round((e.clientX - rect.left)/(rect.right - rect.left)*canvas.width),
-            y: Math.round((e.clientY - rect.top)/(rect.bottom - rect.top)*canvas.height)
-        };
-    }
 
-	function getDir(x1, y1, x2, y2) { 
-		return Math.atan2(x1 - x2, y1 - y2);
+	function angleBetween(x1, y1, x2, y2) { return Math.atan2(x1-x2, y1-y2); }
+	
+	function shapeshift(other) {
+		var s2 = Math.floor(other.size / 2);
+		other.imageData = context.getImageData(other.x - s2, other.y - s2, other.size, other.size);
 	}
- 
+	
+	function move(other) {
+		if (other.nextMove > 0) {
+			other.nextMove--;
+			return;
+		}
+		other.nextMove = Math.round(Math.random() * other.maxMoveDelay - other.minMoveDelay) + other.minMoveDelay;
+		
+		// TODO: speed, persistent direction
+		var direction = Math.random() * Math.PI * 2;
+		other.x += Math.cos(direction);
+		other.y += Math.sin(direction);
+		
+		// stay within the drawing area
+		other.x = Math.min(Math.max(              other.size, other.x), canvas.width  - other.size);
+		other.y = Math.min(Math.max(titleHeight + other.size, other.y), canvas.height - other.size);
+	}
+	
     init();
 };
