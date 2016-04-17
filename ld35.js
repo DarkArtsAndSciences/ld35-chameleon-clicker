0cfd3356e008eee9c2bfc0c97891240611be51dd
Dark Arts and Sciences <darkartsandsciences@gmail.com>
2016-04-16 20:20:18 -0700

    Bugfixes. Shapeshift to / from background works as designed.

--- ld35.js
+++ ld35.js
@@ -1,50 +1,38 @@
 // HTML5 Canvas Basic Game Framework
 // http://rembound.com/articles/how-to-make-a-html5-canvas-game
 window.onload = function() {
+	var titleHeight = 65;
+	var numOthers = 5;
+	var otherSize = 32;
+	var playerSize = 16;
+	var viewDistance = 4;
+	var fov = Math.PI/3;
+	
     var canvas = document.getElementById("viewport"); 
     var context = canvas.getContext("2d");
- 
+	var bufferCanvas;
+	var buffer;
+	var bufferImageData;
     var lastframe = 0;
     var fpstime = 0;
     var framecount = 0;
     var fps = 0;
 	var mousePos = {x:0, y:0};
-	var playerLook = 0;
-	
-	var titleHeight = 65;
-	var bgImageData;
-	
-	var playerSize = 16;
-	var viewDistance = 4;
-	var playerFOV = Math.PI/3;
-	var playerX = (canvas.width - playerSize)/2;
-	var playerY = (canvas.height - playerSize + titleHeight)/2;
-	
+	var lookAt = 0;
+	var playerX;
+	var playerY;
 	var others = [];
- 
-    function init() {
-		spawn();
-		
-        canvas.addEventListener("mousemove", onMouseMove);
-        canvas.addEventListener("mousedown", onMouseDown);
-        canvas.addEventListener("mouseup", onMouseUp);
-        canvas.addEventListener("mouseout", onMouseOut);
-		
-        main(0);
-    }
- 
+	
     function main(tframe) {
         window.requestAnimationFrame(main);
         update(tframe);
         render();
     }
- 
     function update(tframe) {
         var dt = (tframe - lastframe) / 1000;
         lastframe = tframe;
         updateFps(dt);
     }
- 
     function updateFps(dt) {
         if (fpstime > 0.25) {
             fps = Math.round(framecount / fpstime);
@@ -55,42 +43,110 @@ window.onload = function() {
         fpstime += dt;
         framecount++;
     }
+ 
+	function patternGrid(x, y) {
+		var c = 255;
+		if ((x % 16) && (y % 16)) c = 63;
+		else if ((x % 8) || (y % 8)) c = 127;
+		return { r: c, g: c, b: c };
+	}
+	function patternGradient(x, y) {
+		var r = (x % 256);
+		var g = (y % 256);
+		var b = ((x + y) % 4) * 64;
+		return { r: r, g: g, b: b };
+	}
 	
-	function spawn() {
+    function init() {
+		bufferCanvas = document.createElement('canvas');
+		bufferCanvas.width = canvas.width;
+		bufferCanvas.height = canvas.height - titleHeight;
+		buffer = bufferCanvas.getContext("2d");
+		bufferImageData = buffer.getImageData(0, 0, bufferCanvas.width, bufferCanvas.height);
+		
+		playerX = (bufferCanvas.width - playerSize)/2;
+		playerY = (bufferCanvas.height - playerSize)/2;
+		
 		// background pattern
-		bgImageData = context.createImageData(canvas.width, canvas.height - titleHeight);
-		for (var iy = 0; iy < canvas.height - titleHeight; iy++) {
-			for (var ix = 0; ix < canvas.width; ix++) {
-				var i = (ix + iy * bgImageData.width) * 4;
-				bgImageData.data[i+0] = (ix - iy) % 256;  // r
-				bgImageData.data[i+1] = (ix + iy) % 256;  // g
-				bgImageData.data[i+2] = 255;  // b
-				bgImageData.data[i+3] = 255;  // a
+		for (var iy = 0; iy < bufferCanvas.height; iy++) {
+			for (var ix = 0; ix < bufferCanvas.width; ix++) {
+				var c = patternGradient(ix, iy);
+				setPixel(bufferImageData, ix, iy, c.r, c.g, c.b, 255);
 			}
 		}
-		context.putImageData(bgImageData, 0, titleHeight);
+		buffer.putImageData(bufferImageData, 0, 0);
 		
 		// others
-		var size = 8;
-		for (var id=0; id < 23; id++) {
+		for (var id=0; id < numOthers; id++) {
 			others[id] = { 
-				x: Math.floor( Math.random() *  (canvas.width  - size              ) + size                ),
-				y: Math.floor( Math.random() * ((canvas.height - size - titleHeight) + size + titleHeight) ),
-				size: size, 
+				x: Math.floor(Math.random() * canvas.width),
+				y: Math.floor(Math.random() * canvas.height),
+				size: otherSize, 
 				imageData: null,
-				minMoveDelay:  1,  // frames
-				maxMoveDelay: 10,
+				minMoveDelay:  60,  // frames
+				maxMoveDelay: 180,
 				nextMove: 0
 			};
-			move(others[id]);
+			corral(others[id]);
 			shapeshift(others[id]);
 		}
-	}
+		
+        canvas.addEventListener("mousemove", onMouseMove);
+        //canvas.addEventListener("mousedown", onMouseDown);
+        //canvas.addEventListener("mouseup", onMouseUp);
+        //canvas.addEventListener("mouseout", onMouseOut);
+		
+        main(0);
+    }
  
     function render() {
-        // background
-		context.putImageData(bgImageData, 0, titleHeight);
- 
+		// update others in buffer
+		others.forEach(function (other) {
+			var x = other.x - other.size/2;
+			var y = other.y - other.size/2;
+			
+			// http://stackoverflow.com/questions/1878907/the-smallest-difference-between-2-angles
+			var diff = lookAt - (Math.PI*3/2 - angleBetween(playerX, playerY, other.x, other.y));
+			var diffAngle = Math.atan2(Math.sin(diff), Math.cos(diff));
+			
+			if (Math.abs(diffAngle) < fov/2)
+				shapeshift(other);
+			else 
+				move(other);
+		});
+		
+		// background and buffer
+		bufferImageData = buffer.getImageData(0, 0, bufferCanvas.width, bufferCanvas.height);
+		context.putImageData(bufferImageData, 0, titleHeight);
+		
+		// player
+		context.lineWidth = 1;
+		context.strokeStyle = "#dddddd";
+		context.beginPath();
+		context.moveTo(playerX, playerY + titleHeight);
+		context.lineTo(
+			playerX + Math.cos(lookAt + fov/2)*playerSize * viewDistance, 
+			playerY + Math.sin(lookAt + fov/2)*playerSize * viewDistance + titleHeight);
+		context.moveTo(playerX, playerY + titleHeight);
+		context.lineTo(
+			playerX + Math.cos(lookAt - fov/2)*playerSize * viewDistance, 
+			playerY + Math.sin(lookAt - fov/2)*playerSize * viewDistance + titleHeight);
+		context.stroke();
+		
+		context.lineWidth = 3;
+		context.strokeStyle = "red";
+		context.beginPath();
+		context.arc(playerX, playerY + titleHeight, playerSize, lookAt - fov/2, lookAt + fov/2);
+		context.stroke();
+		
+		context.lineWidth = 2;
+		context.fillStyle = "#eeeeee";
+		context.strokeStyle = "black";
+		context.beginPath();
+		context.arc(playerX, playerY + titleHeight, playerSize/2, 0, Math.PI*2);
+		context.fill();
+		context.stroke();
+		
 		// title
         context.fillStyle = "#333333";
         context.fillRect(0, 0, canvas.width, titleHeight);
@@ -103,86 +159,15 @@ window.onload = function() {
         context.fillText("Fps: " + fps, 13, 50);
         context.fillText("Mouse: " + mousePos.x + ", " + mousePos.y, 103, 50);
 		
-		// others
-		others.forEach(function (other) {
-			if (isInView(other.x, other.y)) {
-				shapeshift(other);
-				drawOther(other, "green");
-			} else {
-				move(other);
-				drawOther(other, "red");
-			}
-		});
-		
-		// player
-		drawPlayer();
-    }
-		
-	function isInView(x, y) {
-		var viewDir = Math.PI*3/2 - angleBetween(playerX, playerY, x, y);
-		
-		http://stackoverflow.com/questions/1878907/the-smallest-difference-between-2-angles
-		var diff = playerLook - viewDir;
-		var diffAngle = Math.atan2(Math.sin(diff), Math.cos(diff));
-		
-		return (Math.abs(diffAngle) < playerFOV/2);
+		// mouse cursor
+		context.fillStyle = "red";
+		context.fillRect(mousePos.x, mousePos.y, 4, 4);
 	}
-	
-	function drawOther(other, color) {
-		var s2 = Math.floor(other.size / 2);
-		context.putImageData(other.imageData, other.x - s2, other.y - s2);
-	}
-	
-	function drawPlayer() {
-		context.lineWidth = 1;
-		context.strokeStyle = "#dddddd";
-		context.beginPath();
-		context.moveTo(playerX, playerY);
-		context.lineTo(
-			playerX + Math.cos(playerLook + playerFOV/2)*playerSize * viewDistance, 
-			playerY + Math.sin(playerLook + playerFOV/2)*playerSize * viewDistance);
-		context.moveTo(playerX, playerY);
-		context.lineTo(
-			playerX + Math.cos(playerLook - playerFOV/2)*playerSize * viewDistance, 
-			playerY + Math.sin(playerLook - playerFOV/2)*playerSize * viewDistance);
-		context.stroke();
-		
-		context.lineWidth = 3;
-		context.strokeStyle = "red";
-		context.beginPath();
-		context.arc(playerX, playerY, playerSize, 
-			playerLook - playerFOV/2, 
-			playerLook + playerFOV/2);
-		context.stroke();
-		
-		context.lineWidth = 2;
-		context.fillStyle = "#eeeeee";
-		context.strokeStyle = "black";
-		context.beginPath();
-		context.arc(playerX, playerY, playerSize/2, 0, Math.PI*2);
-		context.fill();
-		context.stroke();
-		
-		context.fillStyle = "#999999";
-		context.fillRect(mousePos.x, mousePos.y, 2, 2);
-	}
-	
-
-    function onMouseMove(e) {
-		var rect = canvas.getBoundingClientRect();
-        mousePos.x = Math.round((e.clientX - rect.left)/(rect.right - rect.left)*canvas.width);
-		mousePos.y = Math.round((e.clientY - rect.top)/(rect.bottom - rect.top)*canvas.height);
-		playerLook = Math.PI*3/2 - angleBetween(playerX, playerY, mousePos.x, mousePos.y);
-	}
-    function onMouseDown(e) {}
-    function onMouseUp(e) {}
-    function onMouseOut(e) {}
-
-	function angleBetween(x1, y1, x2, y2) { return Math.atan2(x1-x2, y1-y2); }
 	
 	function shapeshift(other) {
-		var s2 = Math.floor(other.size / 2);
-		other.imageData = context.getImageData(other.x - s2, other.y - s2, other.size, other.size);
+		var x = other.x - other.size/2;
+		var y = other.y - other.size/2;
+		other.imageData = buffer.getImageData(x, y, other.size, other.size);
 	}
 	
 	function move(other) {
@@ -192,15 +177,56 @@ window.onload = function() {
 		}
 		other.nextMove = Math.round(Math.random() * other.maxMoveDelay - other.minMoveDelay) + other.minMoveDelay;
 		
+		// ghost trail at old location
+		var x = other.x - other.size/2;
+		var y = other.y - other.size/2;
+		buffer.putImageData(other.imageData, x, y);
+		
 		// TODO: speed, persistent direction
 		var direction = Math.random() * Math.PI * 2;
-		other.x += Math.cos(direction);
-		other.y += Math.sin(direction);
+		var speed = other.size;
+		other.x += Math.cos(direction) * speed;
+		other.y += Math.sin(direction) * speed;
 		
-		// stay within the drawing area
-		other.x = Math.min(Math.max(              other.size, other.x), canvas.width  - other.size);
-		other.y = Math.min(Math.max(titleHeight + other.size, other.y), canvas.height - other.size);
+		corral(other);
 	}
+	
+	function corral(other) {
+		// stay within the drawing area
+		other.x = Math.min(Math.max(other.size, other.x), bufferCanvas.width  - other.size);
+		other.y = Math.min(Math.max(other.size, other.y), bufferCanvas.height - other.size);
+	}
+	
+	// http://beej.us/blog/data/html5s-canvas-2-pixel/
+	function setPixel(imageData, x, y, r, g, b, a) {
+		index = (x + y * imageData.width) * 4;
+		imageData.data[index+0] = r;
+		imageData.data[index+1] = g;
+		imageData.data[index+2] = b;
+		imageData.data[index+3] = a;
+	}
+	
+	/*function getPixel(imageData, x, y) {
+		index = (x + y * imageData.width) * 4;
+		return { 
+			r: imageData.data[index+0],
+			g: imageData.data[index+1],
+			b: imageData.data[index+2],
+			a: imageData.data[index+3]
+		}
+	}*/
+
+	function angleBetween(x1, y1, x2, y2) { return Math.atan2(x1-x2, y1-y2); }
+	
+    function onMouseMove(e) {
+		var rect = canvas.getBoundingClientRect();
+        mousePos.x = Math.round((e.clientX - rect.left)/(rect.right - rect.left)*canvas.width);
+		mousePos.y = Math.round((e.clientY - rect.top)/(rect.bottom - rect.top)*canvas.height);
+		lookAt = Math.PI*3/2 - angleBetween(playerX, playerY, mousePos.x, mousePos.y);
+	}
+    //function onMouseDown(e) {}
+    //function onMouseUp(e) {}
+    //function onMouseOut(e) {}
 	
     init();
 };
